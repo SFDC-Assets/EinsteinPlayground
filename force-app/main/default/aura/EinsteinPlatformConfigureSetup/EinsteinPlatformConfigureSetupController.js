9b49cf0cf08c80e48f75facc50e23b9a3eb45f4f
Dennis Schultz <dschultz@dschultz-ltm.internal.salesforce.com>
2020-02-13 14:36:52 -0600

    Fixed bugs that only show in Lightning Debug Mode

--- force-app/main/default/aura/EinsteinPlatformConfigureSetup/EinsteinPlatformConfigureSetupController.js
+++ force-app/main/default/aura/EinsteinPlatformConfigureSetup/EinsteinPlatformConfigureSetupController.js
@@ -68,33 +68,27 @@
     },
     
      handleUploadFinished : function(component, event, helper) {
-
+ 
         var action = component.get("c.updatePemFile");
- 		
-               
         var uploadedFiles = event.getParam("files");
         var contentId = '';
         
         for(var i=0; i<uploadedFiles.length; i++) {
            contentId =  uploadedFiles[i].documentId;
         }
-           component.set("v.pemId", contentId);
-    
-         action.setParams({
+
+        action.setParams({
       		documentId: contentId
     	});
          
     	action.setCallback(this, function(a) {
             if (a.getState() === "SUCCESS") {
-                
                 var certName =  a.getReturnValue();
-                
                	var settings  = component.get("v.settings");
-              //  settings.CertName__c = certName;
                 settings.einsteinplay__CertName__c  = certName;
-                component.set("v.settings", settings);
-                
+                component.set("v.settings", settings);                
              	return;
+
             } else if (a.getState() === "ERROR") {                
     			$A.log("Errors", a.getError());
                 helper.handleErrors(a.getError());
@@ -102,7 +96,6 @@
    		});
 
     	$A.enqueueAction(action);
-
                  
      }
     
