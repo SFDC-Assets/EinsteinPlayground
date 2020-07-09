4d319f4a308f41886bd3d8cd735d70173d91fa91
Dennis Schultz <dschultz@dschultz-ltm.internal.salesforce.com>
2020-07-09 16:38:24 -0500

    EinsteinPlayground and Introduction LWC

--- force-app/main/default/aura/EinsteinPlayground/EinsteinPlaygroundController.js
+++ force-app/main/default/aura/EinsteinPlayground/EinsteinPlaygroundController.js
@@ -1,13 +1,4 @@
 ({
-	changeSpinner: function (component, event, helper) {
-		console.log('changeSpinner');
-    	helper.changeSpinner(component);
-  },
-
-  	updateModelSelection: function (component, event, helper) {
-		console.log('updateModelSelection');
-		helper.updateModelSelection(component, event);
-  	},
 
 	doInitPlayground: function (component, event, helper) {
 		console.log('doInitPlayground');
