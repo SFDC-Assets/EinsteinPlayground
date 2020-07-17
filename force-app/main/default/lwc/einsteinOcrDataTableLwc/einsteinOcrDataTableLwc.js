cd6e5241365212a0a18715bbaddf743489b8e6ea
Dennis Schultz <dschultz@dschultz-ltm.internal.salesforce.com>
2020-07-17 16:52:18 -0500

    First pass

---
+++ force-app/main/default/lwc/einsteinOcrDataTableLwc/einsteinOcrDataTableLwc.js
@@ -0,0 +1,87 @@
+import { LightningElement, api } from 'lwc';
+
+export default class EinsteinOcrDataTableLwc extends LightningElement {
+	data = [];
+	columns = [
+		{ label: 'Label', fieldName: 'label', sortable: true, hideDefaultActions: true },
+		{ label: 'Prob', fieldName: 'probability', type: 'number',sortable: true, hideDefaultActions: true },
+		{ label: 'Min X', fieldName: 'minX', type: 'number',sortable: true, hideDefaultActions: true },
+		{ label: 'Max X', fieldName: 'maxX', type: 'number',sortable: true, hideDefaultActions: true },
+		{ label: 'Min Y', fieldName: 'minY', type: 'number',sortable: true, hideDefaultActions: true },
+		{ label: 'Max Y', fieldName: 'maxY', type: 'number', sortable: true, hideDefaultActions: true },
+		{ label: 'Page', fieldName: 'pageNumber', type: 'number', sortable: true, hideDefaultActions: true }
+	];
+	defaultSortDirection = "asc";
+	sortDirection = "asc";
+	sortedBy;
+	_probabilities;
+
+
+	@api get probabilities() {
+		return this._probabilities;
+	};
+
+	set probabilities(value) {
+		this._probabilities = value;
+
+		let self = this;
+		if (value && value.length > 0) {
+			var probs = JSON.parse(JSON.stringify(value));
+			probs.forEach(function (item, index) {
+				var newItem = {};
+				newItem.index = index;
+				newItem.label = item.label;
+				newItem.probability = item.probability;
+				newItem.minX = item.boundingBox.minX;
+				newItem.maxX = item.boundingBox.maxX;
+				newItem.minY = item.boundingBox.minY;
+				newItem.maxY = item.boundingBox.maxY;
+				// Work around temporary lack of pageNumber attribute
+				newItem.pageNumber = (item.attributes.pageNumber ? item.attributes.pageNumber : '0');
+
+				self.data.push(newItem);
+			})
+
+			// presort by page, then minX, then maxY
+			this.onHandleSort({ detail: { fieldName: 'page', sortDirection: 'asc' } });
+			this.onHandleSort({ detail: { fieldName: 'minX', sortDirection: 'asc' } });
+			this.onHandleSort({ detail: { fieldName: 'maxY', sortDirection: 'asc' } });
+		}
+	}
+
+	hasRendered;
+
+	renderedCallback() {
+		if (!this.hasRendered) {
+			this.hasRendered = true;
+		}
+	}
+	// Used to sort the columns
+	sortBy(field, reverse, primer) {
+	const key = primer
+		? function (x) {
+			return primer(x[field]);
+		}
+		: function (x) {
+			return x[field];
+		};
+
+		return function (a, b) {
+			a = key(a);
+			b = key(b);
+			return reverse * ((a > b) - (b > a));
+		};
+	}
+
+	onHandleSort(event) {
+		const { fieldName: sortedBy, sortDirection } = event.detail;
+		const cloneData = [...this.data];
+
+		cloneData.sort(
+			this.sortBy(sortedBy, sortDirection === "asc" ? 1 : -1)
+		);
+		this.data = cloneData;
+		this.sortDirection = sortDirection;
+		this.sortedBy = sortedBy;
+	}
+}
