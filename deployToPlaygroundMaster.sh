46e95d5b94a474c1d01b4a7468f6dd5e4b6925cc
Dennis Schultz <dschultz@salesforce.com>
2020-08-12 11:57:49 -0500

    Should not be in unmanaged

--- deployToPlaygroundMaster.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-rm -rf mdapioutput
-
-# Convert to metadata format to deploy to non-scratch org
-sfdx force:source:convert -d mdapioutput/
-
-# Deploy to org
-sfdx force:mdapi:deploy -d mdapioutput/ -u PlaygroundMaster -w 100
